4fff36ca4616633eb288750c8bf80328fb559b5b
Janrevolution <vjanrevlance@gmail.com>
2024-02-26 15:53:18 +0800

    Added addition and deletion of doctors

---
+++ README.md
@@ -0,0 +1,9 @@
+##Creation of tables
+
+Create table tbl_doctor(
+    doctor_id varchar(64) primary key,
+    last_name varchar(32),
+    first_name varchar(32),
+    middle_name varchar(32),
+    specialization varchar(32)
+);
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
     "os"
     "bufio"
     "github.com/MasterDimmy/go-cls"
+    "errors"
 )
 
 func customerFunction() {
@@ -17,6 +18,7 @@ func customerFunction() {
 func adminFunction() {
     scanner := bufio.NewScanner(os.Stdin)
     var choice int
+    var err error
 OuterLoop:
     for {
         fmt.Println("\nAdmin Menu:")
@@ -25,11 +27,10 @@ OuterLoop:
         fmt.Println("3. Go back to Main Menu")
         fmt.Print("Enter your choice: ")
         fmt.Scanln(&choice)
-
         switch choice {
         case 1:
             for {
-                err := printRooms()
+                err = printRooms()
                 if err != nil {
                     fmt.Println("Error reading room data:", err)
                 }
@@ -89,8 +90,84 @@ OuterLoop:
                 }
             }
         case 2:
-            fmt.Println("Doctor function is not implemented yet.")
-            // Implement Doctor function logic here
+            for {
+                err = printDoctors()
+                if err != nil {
+                    fmt.Println("Error reading doctor data:", err)
+                }
+
+                fmt.Println("\nDoctor Menu:")
+                fmt.Println("1. Add Doctor")
+                fmt.Println("2. Edit Doctor")
+                fmt.Println("3. Delete room")
+                fmt.Println("4. Go back to Admin Menu")
+                fmt.Print("Enter your choice: ")
+                fmt.Scanln(&choice)
+
+                switch choice {
+                case 1:
+
+                    // To read the whole line, use standard input scanner
+                    var lastName, firstName, middleName, specialization string
+                    
+                    fmt.Print("Enter Dr. Last Name: ")
+                    scanner.Scan()
+                    lastName = scanner.Text()
+                    
+                    fmt.Print("Enter Dr. First Name: ")
+                    scanner.Scan()
+                    firstName = scanner.Text() 
+
+                    fmt.Print("Enter Dr. Middle Name: ")
+                    scanner.Scan()
+                    middleName = scanner.Text() 
+                    
+                    fmt.Print("Enter Dr. Middle Name: ")
+                    scanner.Scan()
+                    specialization = scanner.Text() 
+                    
+                    err := addDoctor(lastName, firstName, middleName, specialization)
+                    if err != nil {
+                        cls.CLS()
+                        fmt.Println("Error creating user:", err)
+                    } else {
+                        cls.CLS()
+                        fmt.Println("User created successfully")
+                    }
+
+                case 2:
+                    fmt.Println("To be edited soon")
+                case 3:
+                    var lastName, firstName, middleName string
+                    
+                    fmt.Print("Enter Dr. Last Name to be deleted: ")
+                    scanner.Scan()
+                    lastName = scanner.Text()
+                    
+                    fmt.Print("Enter Dr. First Name to be deleted: ")
+                    scanner.Scan()
+                    firstName = scanner.Text() 
+
+                    fmt.Print("Enter Dr. Middle Name to be deleted: ")
+                    scanner.Scan()
+                    middleName = scanner.Text() 
+
+                    err := deleteDoctor(lastName, firstName, middleName)
+                    if err != nil {
+                        cls.CLS()
+                        fmt.Println("Error deleting room:", err)
+                    } else {
+                        cls.CLS()
+                        fmt.Println("Room deleted successfully")
+                    }
+
+                case 4:
+                    fmt.Println("Going back to Admin Menu...")
+                    continue OuterLoop 
+                default:
+                    fmt.Println("Invalid choice. Please try again.")
+                }
+            }
         case 3:
             fmt.Println("Going back to Main Menu...")
             return
@@ -201,9 +278,95 @@ func deleteRoom(roomNumber string) error {
     }
     defer db.Close()
 
-    _, err = db.Exec("DELETE FROM tbl_rooms WHERE room_number=?", roomNumber)
+    // Execute the delete operation
+    result, err := db.Exec("DELETE FROM tbl_rooms WHERE room_number=?", roomNumber)
     if err != nil {
         return err
+    }
+
+    rowsAffected, err := result.RowsAffected()
+    if err != nil {
+        return err
+    }
+
+    if rowsAffected == 0 {
+        return errors.New("Room doesn't exist")
+    }
+
+    return nil
+}
+
+
+func printDoctors() error {
+    db, err := connectDB()
+    if err != nil {
+        return err
+    }
+    defer db.Close()
+
+    rows, err := db.Query("SELECT last_name, first_name, middle_name, specialization from tbl_doctor")
+    if err != nil {
+        return err
+    }
+    defer rows.Close()
+
+    for rows.Next() {
+        var lastName, firstName, middleName, specialization string
+
+        err := rows.Scan(&lastName, &firstName, &middleName, &specialization)
+        if err != nil {
+            return err
+        }
+
+        fmt.Printf("Doctor: %s, %s %s Specialization: %s\n", lastName, firstName, middleName, specialization)
+    }
+
+    if err := rows.Err(); err != nil {
+        return err
+    }
+
+    return nil
+}
+
+
+func addDoctor(lastName string, firstName string, middleName string, specialization string) error {
+    db, err := connectDB()
+    if err != nil {
+        return err
+    }
+    defer db.Close()
+
+    uuid := uuid.New().String()
+
+    _, err = db.Exec("INSERT INTO tbl_doctor (doctor_id, last_name, first_name, middle_name, specialization) VALUES (?, ?, ?, ?, ?)", uuid, lastName, firstName, middleName, specialization)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func deleteDoctor(lastName string, firstName string, middleName string) error {
+    db, err := connectDB()
+    if err != nil {
+        return err
+    }
+    defer db.Close()
+
+    // Execute the delete operation
+    result, err := db.Exec("DELETE FROM tbl_doctor WHERE last_name=? AND first_name=? AND middle_name=?", lastName, firstName, middleName)
+    if err != nil {
+        return err
+    }
+
+    // Check if any rows were affected
+    rowsAffected, err := result.RowsAffected()
+    if err != nil {
+        return err
+    }
+
+    if rowsAffected == 0 {
+        return errors.New("Doctor doesn't exist")
     }
 
     return nil
